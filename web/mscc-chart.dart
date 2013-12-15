35b14157d2000b24cf45f1071ffece8daa0283d5
Chris Khoo <chkhoo@groupon.com>
2013-12-14 17:52:34 -0800

    working on height css declaration

--- web/mscc-chart.dart
+++ web/mscc-chart.dart
@@ -7,9 +7,6 @@ import 'ui_filters.dart' show IntToString;
 
 @CustomTag('polymer-mscc-chart')
 class MsccChart extends PolymerElement {
-  DivElement visualization;
-  InputElement slider;
-
   @published int min;
   @published int max;
   @published int yellowFrom;
@@ -19,21 +16,29 @@ class MsccChart extends PolymerElement {
   @published int minorTicks;
   @published int value;
 
+  DivElement visualization;
+  DivElement sliderContainer;
+  InputElement slider;
+
+  Gauge gauge;
+
   MsccChart.created() : super.created();
 
   void enteredView() {
     super.enteredView();
 
     visualization = shadowRoot.querySelector('#gauge');
+    sliderContainer = shadowRoot.querySelector("#slider-container");
     slider = shadowRoot.querySelector("#slider");
 
-    window.onResize.listen(resize);
-    resize(null);
+    window.onResize.listen((_) => resize());
+    resize();
 
     Gauge.load().then((_) {
       int sliderValue() => int.parse(slider.value);
+      int sliderChange(_) => gauge.value = sliderValue();
 
-      Gauge gauge = new Gauge(visualization, "Slider", sliderValue(), {
+      gauge = new Gauge(visualization, "Slider", sliderValue(), {
         'min': min,
         'max': max,
         'yellowFrom': yellowFrom,
@@ -42,12 +47,16 @@ class MsccChart extends PolymerElement {
         'redTo': redTo,
         'minorTicks': minorTicks
       });
-      slider.onChange.listen((_) => gauge.value = sliderValue());
+      slider.onChange.listen(sliderChange);
     });
   }
 
-  void resize(e) {
-    visualization.style.height = '${slider.clientWidth}px';
+  void resize() {
+    int gaugeHeight = this.clientHeight - sliderContainer.marginEdge.height;
+    if (gaugeHeight > 0) {
+      visualization.style.height = '${gaugeHeight}px';
+    }
+    if (gauge != null) gauge.draw();
   }
 
   final Transformer asInteger = new IntToString();
